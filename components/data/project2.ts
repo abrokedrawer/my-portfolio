e89de51a6b3d4f01fc048cca5671a22abb61a513
abrokedrawer37 <noryokusha.37@gmail.com>
2025-06-25 00:39:06 +0530

    more links

--- components/data/project2.ts
+++ components/data/project2.ts
@@ -4,7 +4,7 @@ const project2 = {
   description: 'Full Stack website built with Next.js and Tailwind CSS,allows user to like and comment on posts with all data tracked using mongoDB',
   tags: ['Next.js', 'Tailwind CSS', 'TypeScript', 'ReactJS', 'MongoDB'],
   image: `/projectsPic/Intern03.png`,
-  link:'https://github.com/abrokedrawer/InternWebsite.git'
+  link:'https://github.com/abrokedrawer/InternWebsite'
 };
 
 export default project2;
