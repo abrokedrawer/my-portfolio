d0bb1f484e8a501cee043cffed9fbf253154f967
abrokedrawer37 <noryokusha.37@gmail.com>
2025-06-27 00:02:06 +0530

    css changes

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -23,10 +23,9 @@ export default function MainContent() {
     }
 
     return (
-        <main className="relative flex flex-col items-center justify-center min-h-[60vh] 
-        text-center w-full px-4 sm:px-0 sm:max-w-[55%] mx-auto 
-        border border-gray-800 dark:border-gray-200 rounded-lg p-2 
-        bg-gradient-to-t from-gray-800 to-gray-900">
+        <main className="card relative flex flex-col items-center justify-center min-h-[60vh] 
+        text-center w-full px-4 sm:px-0 sm:max-w-[55%] mx-auto rounded-lg
+        bg-gradient-to-t from-gray-800 to-gray-900 z-index-1">
 
             {/* Home indicator */}
             <span className="absolute -top-3 left-4 bg-white px-2 text-gray-500 text-xl rounded-sm">
--- components/Wavify.tsx
+++ components/Wavify.tsx
@@ -5,22 +5,22 @@ function Wavify() {
   return (
     <div className="relative w-full h-[200px] overflow-hidden bottom-0">
       <Wave className='absolute bottom-0 left-0 w-full h-full'
-        fill='#7595c0'
+        fill='#8861a8'
         paused={false}
         options={{
           height: 30,
-          amplitude: 20,
+          amplitude: 30,
           speed: 0.10,
           points: 4
         }}
         />
       <Wave className='absolute bottom-0 left-0 w-full h-full'
-        fill='#2e425c'
+        fill='#50387d'
         paused={false}
         options={{
-          height: 40,
+          height: 45,
           amplitude: 20,
-          speed: 0.15,
+          speed: 0.25,
           points: 3
         }}
         />
