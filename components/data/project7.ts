65bb05c23e26f036dd7c8a99a4aa0bd406c4e627
abrokedrawer37 <noryokusha.37@gmail.com>
2025-07-01 00:11:41 +0530

    hangman

---
+++ components/data/project7.ts
@@ -0,0 +1,10 @@
+const project6 = {
+  id: 'hangman-v1',
+  title: 'Hangman Replica',
+  description: 'Clone of the word game Hangman',
+  tags: ['Next.js', 'Tailwind CSS', 'Javascript ', 'ReactJS', 'MongoDB'],
+  image: `/projectsPic/hangman01.png`,
+  link:'https://github.com/abrokedrawer/littlelemon'
+};
+
+export default project6;
